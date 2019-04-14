d259fa73ce2d5dfec34659bbec2dae6c35a32771
Elias Johansson <elias.elle.johansson@gmail.com>
2019-04-14 21:56:50 +0200

    program updated to handle multiple excel-documents

--- ReadFromExcel.py
+++ ReadFromExcel.py
@@ -1,22 +1,25 @@
 import xlrd
 
 lena = 0
+mattias = 0
+name = 'linus'
 for n in range(0, 22):
     file_path = 'C:\\Users\\Johan\\Livet\\Utdrag\\'
     if n == 0:
         file_name = 'export.xls'
     else:
         file_name = 'export (' + str(n) + ').xls'
+
     file_path = file_path + file_name
     wb = xlrd.open_workbook(file_path)
     sheet = wb.sheet_by_index(0)
 
     for rows in range(sheet.nrows):
         cell = sheet.cell_value(rows, 1).lower()
-        if 'swish' in cell and 'lena' in cell:
+        if 'swish' in cell and name in cell:
             amount = sheet.cell_value(rows, 3)
             print(cell, 'amount:', sheet.cell_value(rows, 3))
             amount = amount.replace('.', '')
             amount = amount.replace(',', '.')
-            lena += float(amount)
-print('Total lena:', lena)
+            mattias += float(amount)
+print('Total ', name, ':', mattias)
