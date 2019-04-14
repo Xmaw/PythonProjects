343d5a5d721e1be7d4c6dec0bed35a853a43000a
Elias Johansson <elias.elle.johansson@gmail.com>
2019-04-14 21:18:07 +0200

    prints all cells in excel docuemnt

--- ReadFromExcel.py
+++ ReadFromExcel.py
@@ -4,4 +4,6 @@ file_path = 'C:\\Users\\Johan\\PycharmProjects\\export.xls'
 wb = xlrd.open_workbook(file_path)
 sheet = wb.sheet_by_index(0)
 
-print(sheet.cell_value(0,0))
+for x in range(4):
+    for rows in range(sheet.nrows):
+        print(sheet.cell_value(rows, x))
