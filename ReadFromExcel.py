7072d6738dd15a72c3451110283282a16d1d9c2f
Elias Johansson <elias.elle.johansson@gmail.com>
2019-04-14 21:26:21 +0200

    prints all cells containing the sub string 'Swish'

--- ReadFromExcel.py
+++ ReadFromExcel.py
@@ -4,6 +4,7 @@ file_path = 'C:\\Users\\Johan\\PycharmProjects\\export.xls'
 wb = xlrd.open_workbook(file_path)
 sheet = wb.sheet_by_index(0)
 
-for x in range(4):
-    for rows in range(sheet.nrows):
-        print(sheet.cell_value(rows, x))
+for rows in range(sheet.nrows):
+    cell = sheet.cell_value(rows, 1)
+    if 'Swish' in cell:
+        print(cell)
